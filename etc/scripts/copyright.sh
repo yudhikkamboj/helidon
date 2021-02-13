0a2c926b1c6e7ca036ba638b4d081f6eb749082e
Romain Grecourt <romain.grecourt@oracle.com>
2021-02-12 17:47:29 -0800

    troubleshoot copyright

--- etc/scripts/copyright.sh
+++ etc/scripts/copyright.sh
@@ -43,6 +43,9 @@ source ${WS_DIR}/etc/scripts/pipeline-env.sh
 
 die(){ echo "${1}" ; exit 1 ;}
 
+git config -l
+git log -10
+
 mvn ${MAVEN_ARGS} -q org.glassfish.copyright:glassfish-copyright-maven-plugin:copyright \
         -f ${WS_DIR}/pom.xml \
         -Dcopyright.exclude="${WS_DIR}/etc/copyright-exclude.txt" \
